11dd3d4a4eeb34fd7d0b462cf44dea78eeb3adbb
Jinesh Shah <jinsha@microsoft.com>
2020-10-27 16:10:47 +0530

    initial commit

---
+++ OpenPort80.ps1
@@ -0,0 +1,40 @@
+﻿
+
+workflow OpenPort80
+  {
+    param (
+        [Object]$RecoveryPlanContext
+    )
+
+    $Cred = Get-AutomationPSCredential -Name 'AzureCredential'
+
+    # Connect to Azure
+    $AzureAccount = Add-AzureAccount -Credential $Cred
+    $AzureSubscriptionName = Get-AutomationVariable –Name ‘AzureSubscriptionName’
+    Select-AzureSubscription -SubscriptionName $AzureSubscriptionName
+
+    # Specify the parameters to be used by the script
+    $AEProtocol = "TCP"
+    $AELocalPort = 80
+    $AEPublicPort = 80
+    $AEName = "Port 80 for HTTP"
+    $VMGUID = ""
+
+    #Read the VM GUID from the context
+    $VM = $RecoveryPlanContext.VmMap.$VMGUID
+
+    if ($VM -ne $null)
+    {
+        # Invoke pipeline commands within an InlineScript
+
+        $EndpointStatus = InlineScript {
+            # Invoke the necessary pipeline commands to add a Azure Endpoint to a specified Virtual Machine
+            # This set of commands includes: Get-AzureVM | Add-AzureEndpoint | Update-AzureVM (including necessary parameters)
+
+            $Status = Get-AzureVM -ServiceName $Using:VM.CloudServiceName -Name $Using:VM.RoleName | `
+                Add-AzureEndpoint -Name $Using:AEName -Protocol $Using:AEProtocol -PublicPort $Using:AEPublicPort -LocalPort $Using:AELocalPort | `
+                Update-AzureVM
+            Write-Output $Status
+        }
+    }
+  }
